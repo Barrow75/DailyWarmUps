2dcfb109d8ddd36dd825105681636a0ef1ceb555
Ray <105521700+Barrow75@users.noreply.github.com>
2023-07-16 03:40:26 -0600

    Update and rename main.py to Day1.py

--- Day1.py
+++ Day1.py
@@ -1,4 +1,5 @@
 # WarmUp Day 1
+# A very simple pyhton code that asks the user to input 2 numbers and prints out the sum of those numbers
 
 number1 = int(input("Enter a number: "))
 number2 = int(input("Enter a number: "))
